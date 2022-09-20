d411456fcdd45046557ec2b9c59b447ffac56a27
Aditya2252 <113898549+Aditya2252@users.noreply.github.com>
2022-09-20 16:12:29 +0530

    Add files via upload

---
+++ spark/about us.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="aboutUs.css">
+
+
+
+        <div class="AboutUsCard">
+            <!-- Add icon library -->
+            <link rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+            <div class="card">
+                <img src="adi.jpg" alt="John" style="width: 95%">
+
+                <h1>Aditya Daphale</h1>
+                <p class="title">CEO & Founder of Basic Banking System.</p>
+                <p>Web Developer</p>
+                <a href="#"><i class="fa fa-dribbble"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <p><button>Contact</button></p>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+
+
+</body>
+
+</html>
